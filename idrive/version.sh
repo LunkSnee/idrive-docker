6469e836fda81ab0766a3de8090cee26d861bec9
root <root@test-1.snorre.xx>
2026-06-02 19:40:01 +0200

    Error handling - no version found

--- idrive/version.sh
+++ idrive/version.sh
@@ -3,6 +3,11 @@
 VERSION_URL="https://www.idrivedownloads.com/downloads/linux/download-for-linux/linux-backup.script.js"
 VERSION=$(curl -s $VERSION_URL | grep 'var linuxVersion =' | sed 's/.*var linuxVersion = "\([0-9.]*\)".*/\1/')
 
+if [ -z "$VERSION" ]; then
+  echo "Error: iDrive latest version not found"
+  exit 1
+fi
+
 echo "iDrive latest version: $VERSION"
 echo "idrive_version=$VERSION" >> $GITHUB_OUTPUT
-
+exit 0
