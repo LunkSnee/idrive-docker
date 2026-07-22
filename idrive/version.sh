c7fc72bb5c6305665295486aa63958047a026279
Snorre <norbert.kipp@snorre.com>
2026-07-22 19:46:12 +0200

    get iDrive version & URL from new https://www.idrivedownloads.com/downloads/linux/download-for-linux/version-linux.js; use URL in build process with fallback; change APT to mirror:// at built time

--- idrive/version.sh
+++ idrive/version.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-VERSION_URL="https://www.idrivedownloads.com/downloads/linux/download-for-linux/linux-backup.script.js"
-VERSION=$(curl -s $VERSION_URL | grep 'var linuxVersion =' | sed 's/.*var linuxVersion = "\([0-9.]*\)".*/\1/')
+VERSION_URL="https://www.idrivedownloads.com/downloads/linux/download-for-linux/version-linux.js"
+VERSION_JS=$(curl -s $VERSION_URL)
+VERSION=$(echo "$VERSION_JS" | grep '^var linuxScriptVersion =' | sed 's/var linuxScriptVersion = "Version \([0-9.]*\)".*/\1/')
+DL_URL=$(echo "$VERSION_JS" | grep '^var linuxScriptPackageURL = ' | sed "s|var linuxScriptPackageURL = '\(https://.*\)'.*|\1|")
 
-if [ -z "$VERSION" ]; then
-  echo "Error: iDrive latest version not found"
+if [ -z "$VERSION" ] || [ -z "$DL_URL" ]; then
+  echo "Error: iDrive latest version or download URL not found"
   exit 1
 fi
 
 echo "iDrive latest version: $VERSION"
+echo "iDrive download URL: $DL_URL"
 echo "idrive_version=$VERSION" >> $GITHUB_OUTPUT
+echo "idrive_URL=$DL_URL" >> $GITHUB_OUTPUT
 exit 0
